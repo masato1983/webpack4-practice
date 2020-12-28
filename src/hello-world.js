bcf138a11433a0f9efaa876713b765571d1223a0
masato1983 <50264750+masato1983@users.noreply.github.com>
2020-12-28 17:02:35 +0900

    build(lecture-32): code Splitting in Webpack: Multiple JS and CSS Bundles

--- src/hello-world.js
+++ src/hello-world.js
@@ -1,5 +1,15 @@
-function helloworld() {
-  console.log('Hello world');
+import HelloWorldButton from './components/hello-world-button/hello-world-button.js';
+import Heading from './components/heading/heading.js';
+
+const heading = new Heading();
+heading.render();
+const helloWorldButton = new HelloWorldButton();
+helloWorldButton.render();
+
+if (process.env.NODE_ENV === 'production') {
+  console.log('Production mode');
+} else if (process.env.NODE_ENV === 'development') {
+  console.log('Development mode');
 }
 
-export default helloworld;
+// helloWorldButton.methodThatDoesNotExist();
