ece166635a9cf9045c1838cc5f2b0e6bf84f0767
masato1983 <50264750+masato1983@users.noreply.github.com>
2020-12-30 21:56:07 +0900

    build(lecture-36): how To Setup Development Environment For Multiple Page Application

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,9 +3,12 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    'hello-world': './src/hello-world.js',
+    'kiwi': './src/kiwi.js'
+  },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, './dist'),
     publicPath: ''
   },
@@ -63,9 +66,18 @@ module.exports = {
       ]
     }),
     new HtmlWebpackPlugin({
+      filename: 'hello-world.html',
+      chunks: ['hello-world'],
       title: 'Hello world',
-      template: 'src/index.hbs',
-      description: 'Some description'
+      description: 'Some description',
+      template: 'src/page-template.hbs'
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'kiwi.html',
+      chunks: ['kiwi'],
+      title: 'Kiwi',
+      description: 'Kiwi',
+      template: 'src/page-template.hbs'
     })
   ]
 }
