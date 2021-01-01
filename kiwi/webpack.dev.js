72f0b60176cb6285bbbab8ce83207962e227eeba
masato1983 <50264750+masato1983@users.noreply.github.com>
2021-01-01 10:36:27 +0900

    build(lecture-46): creating 2 Separate Applications, Part 2

--- kiwi/webpack.dev.js
+++ kiwi/webpack.dev.js
@@ -3,10 +3,7 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: {
-    'hello-world': './src/hello-world.js',
-    'kiwi': './src/kiwi.js'
-  },
+  entry: './src/kiwi.js',
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, './dist'),
@@ -15,18 +12,12 @@ module.exports = {
   mode: 'development',
   devServer: {
     contentBase: path.resolve(__dirname, './dist'),
-    index: 'index.html',
-    port: 9000,
+    index: 'kiwi.html',
+    port: 9002,
     writeToDisk: true
   },
   module: {
     rules: [
-      {
-        test: /\.(png|jpg)$/,
-        use: [
-          'file-loader'
-        ]
-      },
       {
         test: /\.css$/,
         use: [
@@ -46,7 +37,6 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env'],
-            plugins: ['@babel/plugin-proposal-class-properties']
           }
         }
       },
@@ -66,15 +56,7 @@ module.exports = {
       ]
     }),
     new HtmlWebpackPlugin({
-      filename: 'hello-world.html',
-      chunks: ['hello-world'],
-      title: 'Hello world',
-      description: 'Some description',
-      template: 'src/page-template.hbs'
-    }),
-    new HtmlWebpackPlugin({
       filename: 'kiwi.html',
-      chunks: ['kiwi'],
       title: 'Kiwi',
       description: 'Kiwi',
       template: 'src/page-template.hbs'
