1d969d1f9e4823dfeb202f16c275307c5f70c54e
masato1983 <50264750+masato1983@users.noreply.github.com>
2021-01-01 11:52:39 +0900

    build(lecture-47): setting Up Module Federation

--- hello-world/webpack.dev.js
+++ hello-world/webpack.dev.js
@@ -1,13 +1,14 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
 
 module.exports = {
   entry: './src/hello-world.js',
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, './dist'),
-    publicPath: ''
+    publicPath: 'http://localhost:9001/'
   },
   mode: 'development',
   devServer: {
@@ -56,5 +57,12 @@ module.exports = {
       description: 'Some description',
       template: 'src/page-template.hbs'
     }),
+    new ModuleFederationPlugin({
+      name: 'HelloWorldApp',
+      filename: 'remoteEntry.js',
+      exposes: {
+        './HelloWorldButton': './src/components/hello-world-button/hello-world-button.js'
+      }
+    })
   ]
 }
--- kiwi/webpack.prod.js
+++ kiwi/webpack.prod.js
@@ -2,6 +2,7 @@ const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { ModuleFederationPlugin } = require('webpack').container;
 
 module.exports = {
   entry: './src/kiwi.js',
@@ -65,6 +66,12 @@ module.exports = {
       description: 'Kiwi',
       template: 'src/page-template.hbs',
       minify: false
+    }),
+    new ModuleFederationPlugin({
+      name: 'KiwiApp',
+      remotes: {
+        HelloWorldApp: 'HelloWorldApp@http://localhost:9001/remoteEntry.js'
+      }
     })
   ]
 }
